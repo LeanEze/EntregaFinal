c414f4a63a027661da6de2bf90946ade3f4e94ce
Leandro Jamardo <leandro.jamardo@gmail.com>
2022-07-27 20:36:23 -0300

    actualizacion y coneccion entre templates

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from mi_app import views
-from mi_app.views import mostrar_inicio
+from mi_app.views import ingresar_usuario, mostrar_inicio, mostrar_formulario_adopcion, mostrar_donaciones, mostrar_transito
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', mostrar_inicio, name='inicio'),
-
+    path('adoptar/', mostrar_formulario_adopcion, name='adoptar'),
+    path('transitar/', mostrar_transito, name='transito'),
+    path('donaciones/', mostrar_donaciones, name='donaciones'),
+    path('ingresarusuario/', ingresar_usuario, name='ingresarusuario')
 
 ]
--- mi_app/views.py
+++ mi_app/views.py
@@ -3,5 +3,21 @@ from django.http import HttpResponse
 
 def mostrar_inicio(request):
     
-    return render(request ,"mi_app/index.html", {}) #templeteIndex
+    return render(request ,"mi_app/index.html", {}) #templete Index
 
+def mostrar_formulario_adopcion(request):
+    
+    return render(request ,"mi_app/formularioAdopcion.html", {}) #templete Adopcion
+
+def mostrar_transito(request):
+    
+    return render(request ,"mi_app/transitar.html", {}) #templete transito
+
+
+def mostrar_donaciones(request):
+    
+    return render(request ,"mi_app/donaciones.html", {}) #templete Donaciones
+
+def ingresar_usuario(request):
+    
+    return render(request ,"mi_app/ingresarUsuario.html", {})
