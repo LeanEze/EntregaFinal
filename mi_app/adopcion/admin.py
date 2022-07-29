7a540751b0cf6ac8a3f48a45d58c464bb1b4dbae
Leonardo Bonivardi <lbonivardi2000@gmail.com>
2022-07-29 12:55:45 -0300

    Creacion del rubro adopcion dentro de la app

---
+++ mi_app/adopcion/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from mi_app.adopcion.models import Persona
+
+# Register your models here.
+admin.site.register(Persona)
---
+++ mi_app/adopcion/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
+
+from mi_app.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate, SolicitudUpdate, SolicitudDelete
+
+urlpatterns = [
+    path('index/', login_required(index_adopcion)),
+    path('solicitud/listar', login_required(SolicitudList.as_view()), name='solicitud_listar'),
+    path('solicitud/nueva', login_required(SolicitudCreate.as_view()), name='solicitud_crear'),
+    path('solicitud/editar/<pk>/', login_required(SolicitudUpdate.as_view()), name='solicitud_editar'),
+    path('solicitud/eliminar/<pk>/', login_required(SolicitudDelete.as_view()), name='solicitud_eliminar'),
+]
