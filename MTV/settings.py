261f1b85e011820a74a757f5099352f317997a18
Leandro Jamardo <leandro.jamardo@gmail.com>
2022-08-01 21:01:23 -0300

    historias template terminadas

--- MTV/settings.py
+++ MTV/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mi_app.apps.MiAppConfig',
-    'mi_app.adopcion',
+    
 ]
 
 MIDDLEWARE = [
--- mi_app/adopcion/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from mi_app.adopcion.models import Persona
-
-# Register your models here.
-admin.site.register(Persona)
