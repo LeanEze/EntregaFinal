9f1b32e12e61d1e104aee304378459376f3f3f78
Leandro Jamardo <leandro.jamardo@gmail.com>
2022-08-07 12:46:25 -0300

    actualizacion url

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from mi_app import views
-<<<<<<< HEAD
-from mi_app.views import adopcion_formulario, donaciones_formulario, ingresar_usuario, mostrar_inicio, mostrar_donaciones, crear_usuario, respuesta_adopcion, respuesta_donaciones, respuesta_transito, transito_formulario
-from mi_app.views import password_reset, password_reset_confirm, password_reset_done, password_reset_complete
-=======
-from mi_app.views import adopcion_formulario, ingresar_usuario, mostrar_inicio, mostrar_donaciones, crear_usuario, respuesta_adopcion, respuesta_transito, transito_formulario
+from mi_app.views import adopcion_formulario, donaciones_formulario, ingresar_usuario, mostrar_inicio, crear_usuario, respuesta_adopcion, respuesta_donaciones, respuesta_transito, transito_formulario
 
->>>>>>> dedaf02f8268965ad240505d0f3aadbf6b5e5ca8
+
+
+
 
 
 
